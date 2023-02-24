36098c1456a554600632f0d9f2593c0b010fcbf0
Bilostenko <bilostenkod@gmail.com>
2023-02-24 15:38:58 +0200

    add components, app-info,search-panel

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import "./app-info.css"
+
+const AppInfo = () => {
+  return (
+    <div className="app-info">
+      <h1>Облік співробітників компанії "Ctrl+Alt+Elite" </h1>
+      <h2>Загальна кількість співробітників:</h2>
+      <h2>Премію отрмують:</h2>
+    </div>
+  )
+}
+
+export default AppInfo;
---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,13 @@
+import './search-panel.css'
+
+const SearchPanel = () => {
+  return (
+    <input
+      type="text"
+      placeholder="знайти співробітника"
+      className="form-control search-input"
+    />
+  )
+}
+
+export default SearchPanel;
