4158214fa203e077bc1a32a60140d3404dff9c71
Bilostenko <bilostenkod@gmail.com>
2023-02-25 10:32:39 +0200

    add emp-lis-item

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,5 +1,7 @@
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
+import AppFilter from '../app-filter/app-filter';
+import EmployeesListItem from '../employers-list-item/employers-list-item';
 import './app.css';
 
 function App() {
@@ -9,6 +11,8 @@ function App() {
 
       <div className="search-panel">
         <SearchPanel />
+        <AppFilter />
+        <EmployeesListItem />
       </div>
     </div>
   )
